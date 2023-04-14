1336614127c5d76c7e947227403f6a85cc4bc749
matthewcroughan <matt@croughan.sh>
2023-04-14 03:00:07 +0100

    wip

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
     extra-trusted-public-keys = [ "matthewcroughan.cachix.org-1:fON2C9BdzJlp1qPan4t5AF0xlnx8sB0ghZf8VDo7+e8=" ];
   };
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
+    nixpkgs.url = "github:nixos/nixpkgs/a36fdb5";
     mobile-nixos = {
       url = "github:matthewcroughan/mobile-nixos/71480a82bb5586c66573ad580518e320ee394d39";
       flake = false;
