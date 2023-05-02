dadb6262e1371d6dc20291a83d2792cd8002c87f
matthewcroughan <matt@croughan.sh>
2023-05-02 01:54:52 +0100

    wip

--- flake-modules/phoneImages.nix
+++ flake-modules/phoneImages.nix
@@ -1,5 +1,9 @@
 { self, ... }:
 {
+  flake = { ... }: {
+    # We only ever want to build for arm64, even when the host is x86_64-linux
+    packages.x86_64-linux = self.packages.aarch64-linux;
+  };
   perSystem = { pkgs, ... }: {
     packages = let
       mkImages = list:
